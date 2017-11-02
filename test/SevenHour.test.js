9e684d253b971ab1041953ff2c405a5c53615808
Jeff Bender <j25bender@gmail.com>
2017-11-01 19:57:48 -0600

    initial commit for enzyme testing

---
+++ test/SevenHour.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import SevenHour from '../lib/SevenHour';
+
+
+describe('SevenHour', () => {
+  it('should shallow', () => {
+    // const component = shallow(<SevenHour />);
+
+    // console.log( component.find('input[type="text"]').debug() );
+  })
+})
---
+++ test/TenDay.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import TenDay from '../lib/TenDay';
+
+
+describe('TenDay', () => {
+  it('should shallow', () => {
+    // const component = shallow(<TenDay />);
+
+    // console.log( component.find('input[type="text"]').debug() );
+  })
+})
