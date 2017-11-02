7d06ae33a5bf3f89e0fba681b83fab365d4164cf
Jeff Bender <j25bender@gmail.com>
2017-11-02 12:09:55 -0600

    test

--- test/Welcome.test.js
+++ test/Welcome.test.js
@@ -4,5 +4,9 @@ import Welcome from '../lib/Welcome';
 
 
 describe('Welcome', () => {
-  
+  it('should shallow', () => {
+    // const component = shallow(<Welcome />);
+
+    // console.log( component.find('input[type="text"]').debug() );
+  })
 })
