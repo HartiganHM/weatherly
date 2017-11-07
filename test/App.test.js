ce58cd02bd16de3c428295f42d9f50bf05d4d291
Jeff Bender <j25bender@gmail.com>
2017-11-07 08:53:00 -0700

    from 453 erros to 99 eslint

--- test/App.test.js
+++ test/App.test.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import App from '../lib/App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "../lib/App";
 
+global.localStorage = {
+  getItem(keyword) {
+    if (!global.localStorage[keyword]) {
+      return null;
+    }
+    return JSON.stringify(global.localStorage[keyword]);
+  },
+  setItem(keyword, value) {
+    global.localStorage[keyword] = value;
+  }
+};
 
-describe('App', () => {
-  it('should shallow', () => {
-    // const component = shallow(<App />);
+describe("App", () => {
+  let app;
 
-    // console.log( component.find('input[type="text"]').debug() );
-  })
-})
+  beforeEach(() => {
+    app = shallow(<App />);
+  });
+
+  it("App should exist", () => {
+    expect(app).toBeDefined();
+  });
+
+  it("App should mount <Welcome />", () => {
+    expect(app.find("Welcome")).toBeDefined();
+    expect(app.find("Welcome").length).toEqual(1);
+  });
+
+  it("should be a function", () => {
+    expect(typeof app.state).toEqual("function");
+  });
+
+  it("should render the weather page when a city is specified", () => {
+    app.setState();
+
+    app.setState({ currentWeatherData, tenHourData, sevenDayData });
+    expect(app.find("Search")).toBeDefined();
+    expect(app.find("Search").length).toEqual(1);
+
+    expect(app.find("CurrentWeather")).toBeDefined();
+    expect(app.find("CurrentWeather").length).toEqual(1);
+
+    expect(app.find("TenHour")).toBeDefined();
+    expect(app.find("TenHour").length).toEqual(1);
+
+    expect(app.find("SevenDay")).toBeDefined();
+    expect(app.find("SevenDay").length).toEqual(1);
+  });
+});
